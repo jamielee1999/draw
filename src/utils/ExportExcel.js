5a0c87a8ffb9b7becd57ca4b1cf5315e0a4fd8fd
jamielee1999 <jamielee@guoshipartners.com>
2024-01-17 11:56:41 +0800

    feat: 匯出結果新增表頭

--- src/utils/ExportExcel.js
+++ src/utils/ExportExcel.js
@@ -14,14 +14,20 @@ export function useExcelJS(sheetName, bookType = 'xlsx') {
   function createSheet(workbook, data) {
     const wbName = sheetName || 'SheetJS';
     const sheet = workbook.addWorksheet(wbName);
-    sheet.properties.defaultColWidth = 24;
+    sheet.columns = [
+      { header: '編號', key: 'key', width: 10 },
+      { header: '獎品名稱', key: 'name', width: 50 },
+      { header: '中獎人', key: 'winning_partner', width: 30, outlineLevel: 1 }
+    ];
+
+    sheet.properties.defaultColWidth = 30;
+    sheet.properties.defaultRowHeight = 30;
     sheet.views = [
       {
         state: 'frozen',
         ySplit: 1
       }
     ];
-    // // DATA
     const rowArr = data.map(prizesData =>
       Object.keys(prizesData)
         .map(key => prizesData[key])
@@ -34,7 +40,7 @@ export function useExcelJS(sheetName, bookType = 'xlsx') {
   function getWorkbook(data) {
     const workbook = new ExcelJS.Workbook();
     const now = new Date();
-    workbook.creator = 'OneAD_Ryan';
+    workbook.creator = 'OneAD_JamieLee';
     workbook.created = now;
     workbook.modified = now;
     createSheet(workbook, data);
